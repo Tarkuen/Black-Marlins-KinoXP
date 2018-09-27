69c411f8a091ee8faa1b2ed032fb04f8fbabcaed
Christoffer <christofferhansen20@gmail.com>
2018-09-27 15:42:20 +0200

    ReadAll Showings
    This makes a List<Showings>

--- webapp/src/main/java/blackmarlins/kinoXP/webapp/Model/Showing.java
+++ webapp/src/main/java/blackmarlins/kinoXP/webapp/Model/Showing.java
@@ -1,22 +1,60 @@
 package blackmarlins.kinoXP.webapp.Model;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
 public class Showing {
 
-    String name;
+    int showingId;
+    LocalDateTime showingDateTime;
+    int movieId;
+    int cinemaId;
 
-    public Showing(String name){
-        this.name = name;
+    public Showing(int showingId, LocalDateTime showingDateTime, int movieId, int cinemaId) {
+        this.showingId = showingId;
+        this.showingDateTime = showingDateTime;
+        this.movieId = movieId;
+        this.cinemaId = cinemaId;
     }
 
-    public String getName() {
-        return name;
+    public int getShowingId() {
+        return showingId;
     }
 
-    @Override
+    public void setShowingId(int showingId) {
+        this.showingId = showingId;
+    }
+
+    public LocalDateTime getShowingDateTime() {
+        return showingDateTime;
+    }
+
+    public void setShowingDateTime(LocalDateTime showingDateTime) {
+        this.showingDateTime = showingDateTime;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public int getCinemaId() {
+        return cinemaId;
+    }
+
+    public void setCinemaId(int cinemaId) {
+        this.cinemaId = cinemaId;
+    }
+
+/*    @Override
     public String toString() {
         return "Showing{" +
                 "name='" + name + '\'' +
                 '}';
-    }
+    }*/
 
 }
--- webapp/src/main/java/blackmarlins/kinoXP/webapp/Repository/ShowingsRepository.java
+++ webapp/src/main/java/blackmarlins/kinoXP/webapp/Repository/ShowingsRepository.java
@@ -2,28 +2,27 @@ package blackmarlins.kinoXP.webapp.Repository;
 
 import blackmarlins.kinoXP.webapp.Model.Showing;
 import org.springframework.stereotype.Repository;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.SQLException;
+
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-public class ShowingsRepository implements IShowingRepository{
+public class ShowingsRepository implements IShowingRepository {
 
 
-   private DataConnection dataConnection;
+ private DataConnection databaseConnection;
 
 
-    public ShowingsRepository() throws SQLException {
-       dataConnection = DataConnection.getInstance();
-    }
+ public ShowingsRepository() throws SQLException {
+  databaseConnection = DataConnection.getInstance();
+ }
 
 
-    public DataConnection getDataConnection() {
-        return dataConnection;
-    }
- 
+ public DataConnection getDataConnection() {
+  return databaseConnection;
+ }
+
  @Override
  public void create(Date showing_Date, Integer movie_ID, int cinema_ID) {
 
@@ -51,6 +50,30 @@ public class ShowingsRepository implements IShowingRepository{
 
  @Override
  public List<Showing> readAll(int id) {
-  return null;
+
+  List<Showing> showings = new ArrayList<>();
+  ResultSet rs = null;
+
+  try (Connection conn = DataConnection.getConn()) {
+   PreparedStatement pstms = conn.prepareStatement("SELECT showing_id, showing_datetime, fk_movie_id, fk_cinema_id FROM Showing WHERE fk_movie_id=?;");
+   pstms.setInt(1, id);
+   rs = pstms.executeQuery();
+   while (rs.next()) {
+    Showing s = new Showing(
+            rs.getInt("showing_id"),
+            rs.getTimestamp("showing_datetime").toLocalDateTime(),
+            id,
+            rs.getInt("fk_cinema_id")
+    );
+    showings.add(s);
+   }
+   return showings;
+  } catch (SQLException ex) {
+   ex.printStackTrace();
+
+
+   return null;
+
+  }
  }
 }
